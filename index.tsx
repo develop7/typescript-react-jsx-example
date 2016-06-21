6f5499c6af3650fb73b204d2bb52eca741b8d95e
Andrei Dziahel <develop7@develop7.info>
2016-06-21 16:56:52 +0300

    import valuelink and BOOM!

--- index.tsx
+++ index.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
 
+import Link from "valuelink"
+
 // Render a simple React h1 component into the body.
 React.render(<h1>Hello, TypeScript!</h1>, document.getElementsByTagName('body')[0]);
